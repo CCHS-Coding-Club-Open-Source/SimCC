0c90745e34b71b49aa933506e9132b2a20da3242
czeng18 <32076259+czeng18@users.noreply.github.com>
2017-09-18 15:38:51 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -7,16 +7,18 @@ public class Person
    int[] location = new int[3];
    int age, speed, stressLevel, amountOfSleep;
    boolean license, car;
+   Schedule schedule;
    
-   public Person(String name, int[] loc, int age, int sp, int stress, int sleep, boolean l, boolean c)
+   public Person(String name, int[] loc, int age, int sp, int stress, int sleep, boolean l, boolean c, Schedule s)
    {
-      nameOfPerson = name;
-      location = loc;
-      this.age = age;
-      speed = sp;
-      stressLevel = stress;
+      nameOfPerson  = name;
+      location      = loc;
+      this.age      = age;
+      speed         = sp;
+      stressLevel   = stress;
       amountOfSleep = sleep;
-      license = l;
-      car = c;
+      license       = l;
+      car           = c;
+      schedule      = s;
    }
 }
