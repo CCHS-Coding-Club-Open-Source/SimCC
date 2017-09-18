516206ccb1a9d4f432ab105b72e7cd00c0804965
czeng18 <32076259+czeng18@users.noreply.github.com>
2017-09-18 15:41:10 -0400

    Update Student.java
    More basic variables, and one function

--- Student.java
+++ Student.java
@@ -1,4 +1,18 @@
 public class Student extends Person
 {
    double gpa;
+   int yog, motivationLevel;
+   boolean attendingClass;
+   
+   public Student(double g, int y, int m)
+   {
+      gpa = g;
+      yog = y;
+      motivationLevel = m;
+   }
+   
+   public void skipClass()
+   {
+      attendingClass = false;
+   }
 }
