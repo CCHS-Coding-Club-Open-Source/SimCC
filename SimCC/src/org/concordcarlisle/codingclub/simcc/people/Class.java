502a6593ad119211ce280f4f1c53e94c09dc9227
czeng18 <32076259+czeng18@users.noreply.github.com>
2017-09-25 15:02:59 -0400

    Add Class and Schedule

---
+++ SimCC/src/org/concordcarlisle/codingclub/simcc/people/Class.java
@@ -0,0 +1,11 @@
+package org.concordcarlisle.codingclub.simcc.people;
+
+public class Class {
+	int roomNum;
+	
+	public Class(int r)
+	{
+		roomNum = r;
+	}
+
+}
---
+++ SimCC/src/org/concordcarlisle/codingclub/simcc/people/Schedule.java
@@ -0,0 +1,10 @@
+package org.concordcarlisle.codingclub.simcc.people;
+
+public class Schedule {
+	Class[] classes;
+	
+	public Schedule(Class[] c)
+	{
+		classes = c;
+	}
+}
