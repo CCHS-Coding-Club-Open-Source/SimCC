dd3da0e7e285965137ed6251c434e6574f5b001f
DaWozard <cooperflory@gmail.com>
2017-10-16 15:35:09 -0400

    Working on returning 2d array with both students and rooms
    Working on returning 2d array with both students and rooms

--- SimCC/src/org/concordcarlisle/codingclub/simcc/building/dataObjects/Room.java
+++ SimCC/src/org/concordcarlisle/codingclub/simcc/building/dataObjects/Room.java
@@ -3,9 +3,15 @@ package org.concordcarlisle.codingclub.simcc.building.dataObjects;
 
 public class Room
 {
-	int[] location = new int[3], size = new int[2];
+	int[] location = new int[3], size = new int[2]; 
 	int roomNumber;
 	public Room() {
 		System.out.println("I'm a room!");
+	}
+	public array[][] studentRooms(rooms[], students[]){
+		for (Int a : rooms[]){
+		
+		}
+		}
 	}
 }
